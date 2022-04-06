8d44549adf5f23a2e6438434bb09eff0ca3f4528
Paing Soe Ko <97599513+paingsoeko@users.noreply.github.com>
2022-04-06 22:34:04 +0630

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
 
-function myFunction() {
-  var url = document.getElementById("url");
+var act = document.getElementById("act");
+//Get input url
+var url = document.getElementById("url");
 
-    document.getElementById("demo").innerHTML = url.value;
-    // document.getElementById("widgetApi").style.display="inline";
-    document.getElementById("widgetApi").setAttribute("src", "https://yt-download.org/api/widget?url="+currentVal);
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+const switchMode = document.getElementById('switch-mode');
+
+var format= 'mp3';
+//Change format
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+        btn.onclick = function() {
+            modal.style.display = "block";
+            document.getElementById("buttonApi").setAttribute("src", "https://yt-download.org/api/button/mp3?url="+url.value); 
+                     
+          }
+	} else {
+        btn.onclick = function() {
+            modal.style.display = "block";
+            document.getElementById("buttonApi").setAttribute("src", "https://yt-download.org/api/button/mp4?url="+url.value); 
+        
+          }
+	}
+})
+
+// When the user clicks the button, open the modal 
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+	console.log("hiwindows");
   }
+}
+
+
+
+
+
+
