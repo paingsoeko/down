84fcee3c76ebc1ad8f3cbb11ef0ae07abb70630e
Paing Soe Ko <97599513+paingsoeko@users.noreply.github.com>
2022-04-27 21:23:14 +0630

    Add files via upload

--- app.js
+++ app.js
@@ -1,64 +1,16 @@
-
-var act = document.getElementById("act");
-//Get input url
 var url = document.getElementById("url");
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var filters__active = document.getElementById("projects");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-const switchMode = document.getElementById('switch-mode');
-
-var format= 'mp3';
-//Change format
-// switchMode.addEventListener('change', function () {
-// 	if(this.checked) {
-// 		document.body.classList.add('dark');
-//         btn.onclick = function() {
-//             modal.style.display = "block";
-//             document.getElementById("buttonApi").setAttribute("src", "https://yt-download.org/api/button/mp3?url="+url.value); 
-                     
-//           }
-// 	} else {
-//         btn.onclick = function() {
-//             modal.style.display = "block";
-//             document.getElementById("buttonApi").setAttribute("src", "https://yt-download.org/api/button/mp4?url="+url.value); 
-        
-//           }
-// 	}
-// })
-
-// btn.addEventListener('click', function(){
-//   return document.getElementById("buttonApi").setAttribute("src", "https://yt-download.org/api/widget?url="+url.value); 
-// });
 
 btn.onclick = function() {
-  modal.style.display = "block";
   console.log(url.value);
   document.getElementById("widgetApi").setAttribute("src", "https://yt-download.org/api/widget?url="+url.value); 
-           
+  filters__active.style.height = "120vh";       
 }
 
-// When the user clicks the button, open the modal 
 
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-	console.log("hiwindows");
-  }
-}
 iFrameResize({ log: false, minHeight: 435 }, '#buttonApi')
 
 
